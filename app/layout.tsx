6a99b46a2b34eef41a9261ab715b4d1582a35edd
Mridul24agaga <mridulthareja8@gmail.com>
2025-06-02 14:16:38 +0530

    ASHS

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Goat - Your fav Solana Coin',
+  description: 'Dripped in gold. Drenched in SOL. Its GOAT time.',
 }
 
 export default function RootLayout({
